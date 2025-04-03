d30aa0d31d03548e67afb6b2a79ad058fbe34696
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-03 16:07:34 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 // * SVOLGIMENTO
 
 // * Per 100 volte (ciclo for)
-// *   SE il resto della divisione tra indice e 15 è pari a 0
-// *      Stampo "FizzBuzz"
-// *   SE il resto della divisione tra indice e 5 è pari a 0
-// *      Stampo "Buzz"
-// *   SE il resto della divisione tra indice e 3 è pari a 0
-// *      Stampo "Fizz"
-// *   ALTRIMENTI
-// *      Stampo l'indice del ciclo
+for (let i = 1; i <= 100; i++) {
+  // * SE il resto della divisione tra indice e 15 è pari a 0
+  if (i % 3 === 0 && i % 5 === 0) {
+    // * Stampo "FizzBuzz"
+    console.log("FizzBuzz");
+  }
+  // * SE il resto della divisione tra indice e 5 è pari a 0
+  else if (i % 5 === 0) {
+    // * Stampo "Buzz"
+    console.log("Buzz");
+  }
+  // *SE il resto della divisione tra indice e 3 è pari a 0
+  else if (i % 3 === 0) {
+    // * Stampo "Fizz"
+    console.log("Fizz");
+  }
+  // * ALTRIMENTI
+  else {
+    // * Stampo l'indice del ciclo
+    console.log(i);
+  }
+}
