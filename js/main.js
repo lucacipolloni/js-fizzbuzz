dbf1b30e163bd5044caf507896c0176d83d288f5
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-03 15:52:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// * SVOLGIMENTO
+
+// * Per 100 volte (ciclo for)
+// *   SE il resto della divisione tra indice e 15 è pari a 0
+// *      Stampo "FizzBuzz"
+// *   SE il resto della divisione tra indice e 5 è pari a 0
+// *      Stampo "Buzz"
+// *   SE il resto della divisione tra indice e 3 è pari a 0
+// *      Stampo "Fizz"
+// *   ALTRIMENTI
+// *      Stampo l'indice del ciclo
